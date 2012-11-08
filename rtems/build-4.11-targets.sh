0bf00e2d8061dae538129b80391e2a1b3d705819
Chris Johns <chrisj@rtems.org>
2012-11-09 07:09:51 +1100

    Add rtems-4.11 support using gcc 4.6.3.

---
+++ rtems/build-4.11-targets.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "error: no prefix provided"
+  exit 1
+fi
+
+targets="arm avr bfin h8300 lm32 m32c m32r m68k mips powerpc sh sparc"
+
+log="rtems4.11-build-tools-results.txt"
+
+echo "RTEMS 4.11 Build Results $(date)" > ${log}
+
+for t in ${targets}
+do
+  ../source-builder/sb-set-builder --log=l-${t}.txt \
+                                   --prefix=$1 \
+                                   --target=${t}-rtems4.11 \
+                                   rtems-tools-4.11
+  if [ $? -eq 0 ]; then
+    echo "${t}-rtems4.11 passed" >> ${log}
+  else
+    echo "${t}-rtems4.11 FAILED" >> ${log}
+  fi
+done
