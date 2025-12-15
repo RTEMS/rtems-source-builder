5adf66badc619d81070ca303e41202046f0e8d7c
Chris Johns <chrisj@rtems.org>
2025-12-15 15:40:37 +1100

    sb/download: Only support SHA512 hash
    Updates #140

--- source-builder/sb/download.py
+++ source-builder/sb/download.py
@@ -83,12 +83,7 @@ def _hash_check(file_, absfile, macros, remove=True):
             raise error.internal('invalid hash format: %s' % (file_))
         if hash[0] == 'NO-HASH':
             return not failed
-        try:
-            hashlib_algorithms = hashlib.algorithms
-        except:
-            hashlib_algorithms = [
-                'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'
-            ]
+        hashlib_algorithms = ['sha512']
         if hash[0] not in hashlib_algorithms:
             raise error.general('invalid hash algorithm for %s: %s' %
                                 (file_, hash[0]))
